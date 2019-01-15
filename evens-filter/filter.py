1b081db0225962abb1be545d13ae3f92106b26e0
Michael McCune <msm@redhat.com>
2019-01-15 17:26:31 -0500

    fix error in evens logic

--- evens-filter/filter.py
+++ evens-filter/filter.py
@@ -1,9 +1,9 @@
 import json
 
 def user_defined_function(value):
-    data = int(value) % 2
+    data = int(value)
     ret = None
-    if data == 0:
+    if data % 2 == 0:
         wrapped = {'number': data}
         ret = json.dumps(wrapped)
     return ret
